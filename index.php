5a6f3e96eb9b6b120c0d3cf3a970308a0d4a8f17
CCD311834 <Colinianatkari@gmail.com>
2025-02-27 14:55:25 +0100

    Voorbeeld voor de tabel gemaakt
    Het is bij te passen voor de juiste tabel, want we horen overduidelijk niet Passwords door te geven van takenmakers

--- index.php
+++ index.php
@@ -9,7 +9,31 @@
 <body>
     
     <div class="container">
+    <h1>Users</h1>
+        <a href="taken/create.php">Nieuwe tool &gt;</a>
 
+        <?php
+            require_once 'backend/conn.php';
+            $query = "SELECT * FROM users";
+            $statement = $conn->prepare($query);
+            $statement->execute();
+            $users = $statement->fetchAll(PDO::FETCH_ASSOC);
+        ?>
+
+        <table>
+            <tr>
+                <th>Naam</th>
+                <th>Username</th>
+                <th>Password</th>
+            </tr>
+            <?php foreach($users as $user): ?>
+                <tr>
+                    <td><?php echo $user['naam']; ?></td>
+                    <td><?php echo $user['username']; ?></td>
+                    <td><?php echo $user['password']; ?></td>
+                </tr>
+            <?php endforeach; ?>
+        </table>
 
     </div>
 
